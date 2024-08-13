e9ee854c2e4929fd7e56bcf835f0d9dc88b80e18
Kalia Xiong <kaliaxyooj@gmail.com>
2024-08-12 22:59:44 -0500

    added updatestatus.saga and added updatestatustocompleted function

---
+++ src/redux/sagas/updatestatus.saga.js
@@ -0,0 +1,19 @@
+import { put, takeEvery } from 'redux-saga/effects';
+import axios from 'axios';
+
+
+function* updateStatusToCompleted(action) {
+  try {
+    yield axios.post('/:id/update-status-to-completed', action.payload);
+    yield put({ type: 'SET_STATUS_COMPLETED'}); //from reducer
+    
+
+  } catch (error) {
+    console.log('Error setting media to completed:', error);
+  }
+}
+
+function* updateStatusSaga() {
+    yield takeEvery('UPDATE_STATUS_TO_COMPLETED', updateStatusToCompleted);
+  }
+export default updateStatusSaga;
