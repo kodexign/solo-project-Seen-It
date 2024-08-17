7cb6b302d6fb5b8fe94da79f15a3c0edf11f307a
Kalia Xiong <kaliaxyooj@gmail.com>
2024-08-16 23:19:02 -0500

    updated towatch pages with 4 buttons

--- src/components/ToWatchPage/ToWatchMoviesPage.jsx
+++ src/components/ToWatchPage/ToWatchMoviesPage.jsx
@@ -12,39 +12,63 @@ function ToWatchMoviesPage() {
   useEffect(() => {
     dispatch({ type: 'FETCH_TO_WATCH_MOVIES' }); // type is from movies.saga.js
   }, []);
-  // const handleClick = (id) => {
-  //   history.push(`/details/${id}`);
-  // };
+  
+  //update status to complete
+const handleComplete = (movie) => {
+  const mediaId = movie.id;
+  console.log ('logging mediaId:', mediaId);
+  dispatch({ type: 'UPDATE_STATUS_TO_COMPLETED', payload: {id: mediaId}});
+  console.log ('handleComplete successful');
 
-  const handleDelete = (movie) =>{
-    
-    const mediaId = movie.id;
-    console.log ('logging mediaId:', mediaId);
-    dispatch({ type:'DELETE_MEDIA',  payload: {id: mediaId}});
-    console.log ('handleDelete Successful', mediaId);
-  }
+};
 
-  return (
-    <main>
-    <div className="container">
-      <div>
-        <h2>To Watch Movies List</h2>
-      </div>
-      <section className="movies">
-        {movies.map(movie => {
-          return (
-            <div data-testid='movieItem' key={movie.id}>
-              <p>{movie.title}</p>
-              <div className='statusChangeButtons'>
-              <button  onClick={() => handleDelete(movie)}> Delete </button>
-                <button> Currently Watching</button>
-                <button> Completed </button>
-                <button> DNF </button>
-                </div>
-            </div>
-          );
-        })}
-      </section>
+//update status to did not finish
+const handleDNF = (movie) => {
+  const mediaId = movie.id;
+  console.log ('logging mediaId:', mediaId);
+  dispatch({ type: 'UPDATE_STATUS_TO_DNF', payload: {id: mediaId}});
+  console.log ('handleComplete successful');
+
+};
+//update status to currently watching
+const handleCurrentlyWatching = (movie) => {
+  const mediaId = movie.id;
+  console.log ('logging mediaId:', mediaId);
+  dispatch({ type: 'UPDATE_STATUS_TO_CURRENTLY_WATCHING', payload: {id: mediaId}});
+  console.log ('handleComplete successful');
+
+};
+
+
+//delete media from database
+const handleDelete = (movie) =>{
+  const mediaId = movie.id;
+  console.log ('logging mediaId:', mediaId);
+  dispatch({ type:'DELETE_MEDIA',  payload: {id: mediaId}});
+  console.log ('handleDelete Successful, deleted :', mediaId, movie.title);
+}
+
+return (
+  <main>
+  <div className="container">
+    <div>
+      <h2>To Watch Movies List</h2>
+    </div>
+    <section className="movies">
+      {movies.map(movie => {
+        return (
+          <div data-testid='movieItem' key={movie.id}>
+            <p value={movie.id}>{movie.title}</p>
+            <div className='statusChangeButtons'>
+              <button className="deleteButton" onClick={() => handleDelete(movie)}> Delete </button>
+              <button className="completedButton" onClick={() => handleComplete(movie)}> Completed </button>
+              <button className="currentlyButton" onClick={() => handleCurrentlyWatching(movie)}> Currently Watching</button>
+              <button className="dnfButton" onClick={() => handleDNF(movie)}> DNF </button>
+              </div>
+          </div>
+        );
+      })}
+    </section>
 
     </div>
     </main>
