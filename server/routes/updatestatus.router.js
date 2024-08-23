57877513620fed81011f622bb6e42f34ac1d8201
Kalia Xiong <kaliaxyooj@gmail.com>
2024-08-22 19:47:58 -0500

    updated updatestatus.router to only one put

--- server/routes/updatestatus.router.js
+++ server/routes/updatestatus.router.js
@@ -2,87 +2,108 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-//Update status to complete
-router.put('/update-status-to-completed/:id', (req, res) => {
-    const { id } = req.params;
+router.put('/update-status/:mediaId', (req, res) => {
+    const mediaId = req.params.mediaId;
+    const updateStatusId = req.body.updateStatusId;
+
       const queryText = `
       UPDATE media
       SET status_id = 1
       WHERE id = $1  
       `;
 
-  pool.query(queryText,[id])
+  pool.query(queryText,[updateStatusId, mediaId])
     .then(result => {
       res.send(result.rows);
     })
     .catch(err => {
-      console.log('ERROR: Could not update media to complete', err);
+      console.log(`ERROR: Could not update mediaId: ${mediaId} to statusId: ${updateStatusId}.  Error:`, err);
       res.sendStatus(500)
     })
     
 });
 
-//Update status to Currently Watching
-router.put('/update-status-to-currently_watching/:id', (req, res) => {
-  const { id } = req.params;
-    const queryText = `
-    UPDATE media
-    SET status_id = 2
-    WHERE id = $1  
-    `;
+// //Update status to complete
+// router.put('/update-status-to-completed/:id', (req, res) => {
+//     const { id } = req.params;
+//       const queryText = `
+//       UPDATE media
+//       SET status_id = 1
+//       WHERE id = $1  
+//       `;
 
-pool.query(queryText,[id])
-  .then(result => {
-    res.send(result.rows);
-  })
-  .catch(err => {
-    console.log('ERROR: Could not update media to currently watching', err);
-    res.sendStatus(500)
-  })
+//   pool.query(queryText,[id])
+//     .then(result => {
+//       res.send(result.rows);
+//     })
+//     .catch(err => {
+//       console.log('ERROR: Could not update media to complete', err);
+//       res.sendStatus(500)
+//     })
+    
+// });
+
+// //Update status to Currently Watching
+// router.put('/update-status-to-currently_watching/:id', (req, res) => {
+//   const { id } = req.params;
+//     const queryText = `
+//     UPDATE media
+//     SET status_id = 2
+//     WHERE id = $1  
+//     `;
+
+// pool.query(queryText,[id])
+//   .then(result => {
+//     res.send(result.rows);
+//   })
+//   .catch(err => {
+//     console.log('ERROR: Could not update media to currently watching', err);
+//     res.sendStatus(500)
+//   })
   
-});
+// });
 
-//Update status to To Watch
-router.put('/update-status-to-watch/:id', (req, res) => {
-  const { id } = req.params;
-  console.log('id:', id);
-  console.log('type of', typeof id);
-    const queryText = `
-    UPDATE media
-    SET status_id = 3
-    WHERE id = $1  
-    `;
+// //Update status to To Watch
+// router.put('/update-status-to-watch/:id', (req, res) => {
+//   const { id } = req.params;
+//   console.log('id:', id);
+//   console.log('type of', typeof id);
+//     const queryText = `
+//     UPDATE media
+//     SET status_id = 3
+//     WHERE id = $1  
+//     `;
 
-pool.query(queryText,[id])
-  .then(result => {
-    res.send(result.rows);
-  })
-  .catch(err => {
-    console.log('ERROR: Could not update media to to watch', err);
-    res.sendStatus(500)
-  })
+// pool.query(queryText,[id])
+//   .then(result => {
+//     res.send(result.rows);
+//   })
+//   .catch(err => {
+//     console.log('ERROR: Could not update media to to watch', err);
+//     res.sendStatus(500)
+//   })
   
-});
+// });
 
-//Update status to did not finish
-router.put('/update-status-to-dnf/:id', (req, res) => {
-  const { id } = req.params;
-    const queryText = `
-    UPDATE media
-    SET status_id = 4
-    WHERE id = $1  
-    `;
+// //Update status to did not finish
+// router.put('/update-status-to-dnf/:id', (req, res) => {
+//   const { id } = req.params;
+//     const queryText = `
+//     UPDATE media
+//     SET status_id = 4
+//     WHERE id = $1  
+//     `;
 
-pool.query(queryText,[id])
-  .then(result => {
-    res.send(result.rows);
-  })
-  .catch(err => {
-    console.log('ERROR: Could not update media to to watch', err);
-    res.sendStatus(500)
-  })
+// pool.query(queryText,[id])
+//   .then(result => {
+//     res.send(result.rows);
+//   })
+//   .catch(err => {
+//     console.log('ERROR: Could not update media to to watch', err);
+//     res.sendStatus(500)
+//   })
   
-});
+// });
 
 router.delete('/:id', (req, res) => {
   const { id } = req.params;
