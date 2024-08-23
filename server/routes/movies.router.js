db50976aa5ce013446a7b8d7a25d92055c529db1
Kalia Xiong <kaliaxyooj@gmail.com>
2024-08-22 19:41:06 -0500

    updates movies router to only one get route

--- server/routes/movies.router.js
+++ server/routes/movies.router.js
@@ -2,8 +2,10 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-// GET movies with Status of Completed
-router.get('/completed-movies', (req, res) => {
+
+router.get('/movies/:currentStatusId', (req, res) => {
+  const currentStatusId = req.params.currentStatusId;
+
   const queryText = `
   SELECT media.id, media.title, media.platform 
   FROM media
@@ -11,71 +13,90 @@ router.get('/completed-movies', (req, res) => {
   WHERE media.movie = true AND status.type = 'completed' 
   ORDER BY media.title ASC;
   `;
-pool.query(queryText)
+pool.query(queryText, [currentStatusId])
   .then(result => {
     res.send(result.rows);
   })
   .catch(err => {
-    console.log('ERROR: Get completed-movies', err);
+    console.log('ERROR: Get movies', err);
     res.sendStatus(500)
   })
 });
 
-// GET movies with status of currently watching
-router.get('/currently-watching-movies', (req, res) => {
-  const queryText = `
-  SELECT media.id, media.title, media.platform 
-  FROM media
-  JOIN status ON media.status_id = status.id
-  WHERE media.movie = true and status.type = 'currently_watching';
-  `;
-pool.query(queryText)
-  .then(result => {
-    res.send(result.rows);
-  })
-  .catch(err => {
-    console.log('ERROR: Get currently-watching-movies', err);
-    res.sendStatus(500)
-  })
-  });
+// // GET movies with Status of Completed
+// router.get('/completed-movies', (req, res) => {
+//   const queryText = `
+//   SELECT media.id, media.title, media.platform 
+//   FROM media
+//   JOIN status ON media.status_id = status.id
+//   WHERE media.movie = true AND status.type = 'completed' 
+//   ORDER BY media.title ASC;
+//   `;
+// pool.query(queryText)
+//   .then(result => {
+//     res.send(result.rows);
+//   })
+//   .catch(err => {
+//     console.log('ERROR: Get completed-movies', err);
+//     res.sendStatus(500)
+//   })
+// });
 
-  //GET movies with status of to watch
-router.get('/to-watch-movies', (req, res) => {
-  const queryText = `
-  SELECT media.id, media.title, media.platform 
-  FROM media
-  JOIN status ON media.status_id = status.id
-  WHERE media.movie = true and  status.type = 'to_watch'
-  ORDER BY media.title ASC;
-  `;
-pool.query(queryText)
-  .then(result => {
-    res.send(result.rows);
-  })
-  .catch(err => {
-    console.log('ERROR: Get to-watch-movies', err);
-    res.sendStatus(500)
-  })
-  });
+// // GET movies with status of currently watching
+// router.get('/currently-watching-movies', (req, res) => {
+//   const queryText = `
+//   SELECT media.id, media.title, media.platform 
+//   FROM media
+//   JOIN status ON media.status_id = status.id
+//   WHERE media.movie = true and status.type = 'currently_watching';
+//   `;
+// pool.query(queryText)
+//   .then(result => {
+//     res.send(result.rows);
+//   })
+//   .catch(err => {
+//     console.log('ERROR: Get currently-watching-movies', err);
+//     res.sendStatus(500)
+//   })
+//   });
 
-//GET movies with status of did not finish
-router.get('/dnf-movies', (req, res) => {
-  const queryText = `
-  SELECT media.id, media.title, media.platform 
-  FROM media
-  JOIN status ON media.status_id = status.id
-  WHERE media.movie = true and  status.type = 'did_not_finish'
-  ORDER BY media.title ASC;
-  `;
-pool.query(queryText)
-  .then(result => {
-    res.send(result.rows);
-  })
-  .catch(err => {
-    console.log('ERROR: GET dnf-movies', err);
-    res.sendStatus(500)
-  })
-  });
+//   //GET movies with status of to watch
+// router.get('/to-watch-movies', (req, res) => {
+//   const queryText = `
+//   SELECT media.id, media.title, media.platform 
+//   FROM media
+//   JOIN status ON media.status_id = status.id
+//   WHERE media.movie = true and  status.type = 'to_watch'
+//   ORDER BY media.title ASC;
+//   `;
+// pool.query(queryText)
+//   .then(result => {
+//     res.send(result.rows);
+//   })
+//   .catch(err => {
+//     console.log('ERROR: Get to-watch-movies', err);
+//     res.sendStatus(500)
+//   })
+//   });
+
+// //GET movies with status of did not finish
+// router.get('/dnf-movies', (req, res) => {
+//   const queryText = `
+//   SELECT media.id, media.title, media.platform 
+//   FROM media
+//   JOIN status ON media.status_id = status.id
+//   WHERE media.movie = true and  status.type = 'did_not_finish'
+//   ORDER BY media.title ASC;
+//   `;
+// pool.query(queryText)
+//   .then(result => {
+//     res.send(result.rows);
+//   })
+//   .catch(err => {
+//     console.log('ERROR: GET dnf-movies', err);
+//     res.sendStatus(500)
+//   })
+//   });
 
   
   
